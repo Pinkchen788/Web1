eb65dadf368b1239b5652acef07aaea67f6e4d04
sutadta.chanparn <pinkchanparn@gmail.com>
2024-04-27 11:21:42 +0700

    add iris df

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+df = pd.read_csv(url, header=None)
+df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 
 # hello world
 st.title('Hello World')
 st.write('This is a test')
+st.dataframe(df)
